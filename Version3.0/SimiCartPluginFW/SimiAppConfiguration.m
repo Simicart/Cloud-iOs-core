dfd51ac79b9d5d5359fc44a0db900acd780f09ea
Liam <congnghiep.dao@trueplus.vn>
2016-02-24 14:40:56 +0700

    Update app Config

--- Version3.0/SimiCartPluginFW/SimiAppConfiguration.m
+++ Version3.0/SimiCartPluginFW/SimiAppConfiguration.m
@@ -24,45 +24,14 @@
 NSString *const DEMO_MODE               = @"YES";
 
 
-NSString *const kSimiKey                = @"ef1486cf81657cf2fde4dca2a3ecd7c4ac5a7cb0";
-NSString *kBaseURL                      = @"http://dev-api.jajahub.com/rest/";
-
-//NSString *const kSimiKey                = @"f4fb6d3ab442ed57b3d0b73b0aee31f3";
-//NSString *kBaseURL                      = @"http://www.betastore.me/hebrew/";
+//NSString *const kSimiKey                = @"5ed6a808a6142d936196cb4f69a781f7af02fb3a";
+//NSString *kBaseURL                      = @"http://dev-api.jajahub.com/rest/";
+//NSString *const kSimiKey                = @"43d4ba3f7c8ecbba2606233b82e237df67a4b361";
 
 
-//NSString *kBaseURL                = @"http://demo.magestore.com/simicart/simipos/index.php/";
-//NSString *const kSimiKey          = @"14e98759df0df471e5f7b44ead9ab36d";
+NSString *const kSimiKey                = @"43d4ba3f7c8ecbba2606233b82e237df67a4b361";
+NSString *kBaseURL                      = @"http://api.jajahub.com/rest/";
 
-//NSString *kBaseURL                = @"http://demo.magestore.com/simicart/simipos1/index.php";
-//NSString *const kSimiKey          = @"5cd44fc5691c61abc496c0cb4c5a227c";
-
-//NSString *kBaseURL                = @"http://www.dealsagora.com/";
-//NSString *const kSimiKey          = @"6c769928970db39af2b2a42e4631a96a";
-
-//NSString *kBaseURL                      = @"http://www.carlocivera.org/";
-//NSString *const kSimiKey                = @"579dbf22a52ca67b5b272ad78be3a12c";
-
-//NSString *kBaseURL                      = @"http://mirigrushek.kz/index.php/";
-//NSString *const kSimiKey                = @"a91641e05456fcb1b08f8ab8d2afba65";
-
-//NSString *kBaseURL                      = @"http://shop.mrmrsitaly.it/";
-//NSString *const kSimiKey                = @"9b83728fedd8228874529e0a28f6c608";
-
-//NSString *kBaseURL                      = @"http://dev-shopping.simicart.com/";
-//NSString *const kSimiKey                = @"5630f016e2bc736e4b11af5a";
-
-//NSString *kBaseURL                      = @"http://www.sportlet.it/";
-//NSString *const kSimiKey                = @"49b26660a65b3936bdb58dceb1179bf6";
-
-//NSString *kBaseURL                      = @"http://dev-shopping.simicart.com/";
-//NSString *const kSimiKey                = @"5630f705e2bc737f5411af5e";
-
-//NSString *kBaseURL                      = @"http://shopping.simicart.com/";
-//NSString *const kSimiKey                = @"563788c5e2bc73065f7b23c6";
-
-//NSString *kBaseURL                        = @"http://172.28.162.126/magento01/index.php/";
-//NSString *const kSimiKey                  = @"628fcb97a9d1546dcf98b20b59c724e4";
 
 //Theme Color
 NSString *kThemeColorHex                = @"#faa525";
